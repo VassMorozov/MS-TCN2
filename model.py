c472b211b11a60af4422b95c5bbdaaf2e5225734
VassMorozov <morozov6@gmail.com>
2022-07-26 20:50:26 +0100

    Update model.py

--- model.py
+++ model.py
@@ -224,7 +224,7 @@ class Trainer:
                 _, predicted = torch.max(predictions[-1].data, 1)
                 
                 correct += (predicted == input_y).sum()
-                print(input_y.size())
+                
                 total += input_y.size()[1]
                 
                 predicted = predicted.squeeze()
